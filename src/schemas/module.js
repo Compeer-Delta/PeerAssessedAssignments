e68edf4e4d6e78a0932dbf192f49c0e1bd51349f
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-01-25 12:04:49 +0000

    Update on Module.js schema

--- src/schemas/module.js
+++ src/schemas/module.js
@@ -1,17 +1,24 @@
 //user at later data or remove entirely
 //ref: https://discord.com/channels/1023971333706629200/1025103915508838480/1049676345514393650
+
+//Last update: 25/01, jd750
 const { Schema, model } = require("mongoose");
 const moduleSchema = new Schema({
   _id: Schema.Types.ObjectId,
-  userId: {
+  moduleId: {
     type: String,
     required: true,
     default: null,
   },
-  enrolledStudents: {
-    type: [],
-    required: false,
-    default: [],
+  moduleName: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  institution: {
+    type: String,
+    required: true,
+    default: null
   },
   enrolledTeachers: {
     type: [],
