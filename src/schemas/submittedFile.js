693b519b1733b77c08fdbd3cdfa1f902277f8321
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-01-25 12:52:55 +0000

    Updates to submittedFile schema (marker details)

--- src/schemas/submittedFile.js
+++ src/schemas/submittedFile.js
@@ -1,14 +1,15 @@
 //Schema can be used to store the data of the file submitted by a user
 
 //File handling needs to be looked into further, GridFS only used for >16MB
+//Work is not allocated to someone and not marked by default, hence the null and false default values
 
 const { Schema, model } = require("mongoose");
 const moduleSchema = new Schema({
     _id: Schema.Types.ObjectId,
     userId: {
-      type: String,
-      required: true,
-      default: null,
+        type: String,
+        required: true,
+        default: null,
     }, 
     moduleId: {
         type: String,
@@ -19,6 +20,16 @@ const moduleSchema = new Schema({
         type: Buffer,
         required: true,
         default: null,
+    },
+    markerId: {
+        type: String,
+        required: true,
+        default: null,
+    },
+    marked: {
+        type: Boolean,
+        required: true,
+        default: false,
     }
 });
 
