0f2c1fd5e884e17818b61d9d69fa90e465376f12
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-01-27 15:15:51 +0000

    Refactoring DB querying to an async function

--- src/functions/tools/registered.js
+++ src/functions/tools/registered.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 //this finds the account
-let userAccount = await User.findOne({ userId: interaction.user.id });
 
-//if user not found
-if (!userAccount) {
-    //create user entry in DB
-    userAccount = await new User({
-        _id: mongoose.Types.ObjectId(),
-        userId: interaction.user.id,
-        userPassword: "Password123",
-    });
-    await userAccount.save().catch(console.error);
-}
+async function findRegistered(givenID) {
+    let userAccount = await User.findOne({ userId: givenID });
+
+    //if user not found
+    if (!userAccount) {
+        //create user entry in DB
+        userAccount = await new User({
+            _id: mongoose.Types.ObjectId(),
+            userId: interaction.user.id,
+            userPassword: "Password123",
+        });
+        await userAccount.save().catch(console.error);
+    }
+};
+
+module.exports = findRegistered
