8481dd560933d09089d96f98874fb93c44b55c83
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-02-01 11:39:04 +0000

    FileUpload.jsx created

---
+++ src/pages/FileUpload.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import FileUploader from '../components/FileUploader';
+import HeroSection from '../components/HeroSection';
+import StudentView from '../pages/StudentView';
+
+function FileUpload() {
+  return (
+    
+    <>
+    <HeroSection></HeroSection>
+    <FileUploader></FileUploader>
+    </>
+    
+  )
+}
+
+export default FileUpload
