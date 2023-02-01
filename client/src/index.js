8fd48e12075042b1690668b48aeee77ea7ed9300
Vendron <40866135+Vendron@users.noreply.github.com>
2023-02-01 15:57:39 +0000

    Add env + Add Schemas to api

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-require("dotenv").config();
-const { DBtoken } = process.env;
-const { connect } = require("mongoose");
-const app = express();
-app.listen(5000)
-(async () => {
-    await connect(DBtoken).catch(console.error);
-  })();
