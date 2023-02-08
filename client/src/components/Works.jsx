0d24b7092387d0085a3184ea68b3cd879b5d0087
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-02-08 17:00:11 +0000

    Quick URL change; is a static URL now

--- client/src/components/Works.jsx
+++ client/src/components/Works.jsx
@@ -14,7 +14,7 @@ function Works() {
                               imgUrl={work.imgUrl}
                               title={work.title}
                               tech={work.tech}
-                              workUrl={work.workUrl + "/" + work.id}
+                              workUrl={"/upload/" + work.id}
                               dueDate={work.dueDate}
                               setDate={work.setDate}>
                     </WorkItem>
