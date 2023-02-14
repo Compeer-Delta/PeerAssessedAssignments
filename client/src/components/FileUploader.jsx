c5084443828b423ae2629eb6992b2277959ff0be
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-02-14 16:54:33 +0000

    Increased functionality in FileUploader component

--- client/src/components/FileUploader.jsx
+++ client/src/components/FileUploader.jsx
@@ -1,72 +1,56 @@
-import React from 'react'
-import { useState, useRef } from 'react';
+import React from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import works from '../data/works';
 
 
 function FileUploader(props) {
 
-    const [file, setFile] = useState({fileName:'', fileContent:'' });
+    const [file, setFile] = useState({});
+    const [assignment, setAssignmentDetails] = useState({});
+    //useState({fileName:'', fileContent:'' });
     const [dragActive, setDragActive] = useState(false);
 
     const inputRef = useRef(null);
-    const currentFileRef = useRef(null);
+    const aDetails = useRef(null);
+    const fDetails = useRef(null);
 
-    const submissionID = useParams();
+    const params = useParams();
     const userData = JSON.parse(sessionStorage.getItem('loginSessionData'));
 
-    //Debugging
-    console.log(submissionID.id);
-    console.log(userData);
-
-    const handleFile = e => {
+    const handleFile = function(e) {
         {
-        // const file = e[0];
-        // const reader = new FileReader();
-        // reader.readAsText(file);
-        // reader.onload = () => {
-        //     setFile({fileName: file.name, fileContent: reader.result});
-            
-        //   {props.UploadedData(reader.result.split(","))}
-        
-        // }
-        // reader.onerror= () => {
-        //     console.log("file error", reader.error)
-        // }
-        }
-
-        const file = e[0];
-        console.log(file); //Debugging
-
-        
-
         //HATHAN contrib:
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = () => {
+        // const reader = new FileReader();
+        // reader.readAsText(e[0]);
+        // reader.onload = () => {
         
-        setFile({fileName: file.name, fileContent: reader.result});
-        console.log("UT: " + props.uploadType);
-        // if ((((fileContent.split(/\n/))[0]).split(",")).length() == 4) //checks if its formatted in account data
-        if (props.uploadType === "modules") {
-                props.UploadedData((reader.result).split(","))
-        }
-        else if (props.uploadType === "accounts") {
-            if ((((((reader.result).split(/\n/))[0]).split(",")).length == 5)) {
-                {props.UploadedData(((reader.result).split(/\n/)))}
-            }
-            else { 
-                console.log("error with size of row not equal to 4 : " +(((((reader.result).split(/\n/))[0]).split(",")).length) )
-            }
-        }
+        //setFile({fileName: e[0].name, fileContent: reader.result});
+        // console.log("UT: " + props.uploadType);
+        // // if ((((fileContent.split(/\n/))[0]).split(",")).length() == 4) //checks if its formatted in account data
+        // if (props.uploadType === "modules") {
+        //         props.UploadedData((reader.result).split(","))
+        // }
+        // else if (props.uploadType === "accounts") {
+        //     if ((((((reader.result).split(/\n/))[0]).split(",")).length == 5)) {
+        //         {props.UploadedData(((reader.result).split(/\n/)))}
+        //     }
+        //     else { 
+        //         console.log("error with size of row not equal to 4 : " +(((((reader.result).split(/\n/))[0]).split(",")).length) )
+        //     }
+        // }
         
-        }
+        // }
 
-        reader.onerror= () => {
-            console.log("file error", reader.error);
-        }
+        // reader.onerror= () => {
+        //     console.log("file error", reader.error);
+        // }
         //END HATHAN contrib.
+        }
 
+        setFile(e[0]);
         
+
         //GREG: Please put MongoDB queries for: object insert for the submittedFile, returning if user is part of this module, returning if there is a record of them submitting already.
         //(I will do the if statements and everything else)
 
@@ -97,7 +81,7 @@ function FileUploader(props) {
         }
     };
 
-    //Handles button/label click
+    //Handles any change w/in the upload area
     const handleChange = function(e) {
         e.preventDefault();
         if (e.target.files && e.target.files[0]) {
@@ -105,12 +89,48 @@ function FileUploader(props) {
         }
     };
 
+    //Handles button/label click
     const onButtonClick = () => {
         inputRef.current.click();
     };
 
+    const getAssignmentDetails = function(id) {
+        const worksArray = Object.values(works);
+
+        for(let i = 0; i < worksArray.length; i++) {
+            let w = worksArray[i];
+
+            if(w.id == id) {
+                return w;
+            }
+        }
+    };
+
+    //Page Loading single-calls
+    useEffect(() => {
+        const onPageLoad = () => {
+            //Debugging
+
+            console.log(params.id);
+            setAssignmentDetails(getAssignmentsDetails(params.id));
+            console.log(getAssignmentDetails(params.id));
+            // //console.log(userData);
+        };
+
+        if(document.readyState === 'complete') {
+            onPageLoad();
+        } else {
+            window.addEventListener('load', onPageLoad);
+            return () => window.removeEventListener('load', onPageLoad);
+        }
+    }, []);
+    
     return (
         <div id="uploadContainer">
+            <div id="assignmentDetails" ref={aDetails}>
+            ASSIGNMENT DETAILS HERE
+            </div>
+
             <form id="uploadForm" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
                 <input ref={inputRef} type="file" id="uploadInput" onChange={handleChange}/>
                 <label id="uploadLabel" hmtlfor="uploadInput" className={dragActive ? "drag-active" : "" }>
@@ -121,6 +141,10 @@ function FileUploader(props) {
                 </label>
                 { dragActive && <div id="dragElement" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
             </form>
+
+            <div id="fileDetails" ref={fDetails}>
+            FILE DETAILS HERE
+            </div>
         </div> 
     )
 }
