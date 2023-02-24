c95dc895599d039497f0461320b49757ab0f9991
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-02-24 17:43:03 +0000

    Create temporary notif file (for testing)

---
+++ client/src/data/tempNotifs.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        messageId: "101",
+        userId: "999",
+        notifTitle: "Work Due!",
+        notifContent: "Class work 3 is due in a week's time!",
+        urgency: true,
+    },
+    {
+        messageId: "102",
+        userId: "999",
+        notifTitle: "New assignment added",
+        notifContent: "A new assignment has been set for COMP6000. See the module page for more details.",
+        urgency: false,
+    },
+];
+
+//Simulating the database data - last changed 24/02, jd750
