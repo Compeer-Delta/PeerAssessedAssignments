c4ef756d85b78f49658bdca7dedc43e21b865c7d
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-02-27 14:33:07 +0000

    Tentative SV changes

--- client/src/pages/StudentView.jsx
+++ client/src/pages/StudentView.jsx
@@ -9,64 +9,95 @@ import LoginCard from '../components/LoginCard';
 import Works from '../components/Works'
 import ViewFeedback from './ViewFeedback';
 import Notifications from './Notifications';
+import temporaryModulesData from '../data/temporaryModulesData';
 
 function StudentView({title}) {
-    const [toggle, setToggle] = useState(true);
-    const [isShown, setIsShown] = useState(false);
+  // const [toggle, setToggle] = useState(true);
+  // const [isShown, setIsShown] = useState(false);
+  const [module, setModuleDetails] = useState({});
 
 
-    const toggleClass = <SubmitWork/>;
-
-   // const {type}  = useParams();
-  //  const stateParamVal = useLocation().state.stateParam
-
- // console.log(type + "test ");
+  const toggleClass = <SubmitWork/>;
+  // console.log(type + "test ");
   const location = useLocation();
-  const {moduleTitle} = location.state;
+  // const {moduleTitle} = location.state;
   const {nestedPage} = location.state;
+
+  const params = useParams();
   
+  const getModuleDetails = function(id) {
+    const moduleArray = Object.values(temporaryModulesData);
 
-    return(
-        <>
-    
- <HeroSection prevPageName = "Modules" prevUrl= "/modules"></HeroSection>
- 
-          <div className = 'font-Dosis bg:white dark:bg-gray-400'>
-            <div className='max-w-5xl mx-auto w-11/12'>
-              
-              <>
-                <SideBar moduleTitle={moduleTitle}> </SideBar>
-                
-              
-                {false === false ? ( // CHANGE TO ONLY APPEAR IF TEACHER !!!!!!!
-                  <Link to= "/addassignment" className='sidebar fixed lg:left-0 text-gray-300 dark:text-gray-300 p-3 my-80 ml-2 py-6 bg-slate-800 dark:bg-zinc-700 w-72 h-20 overflow-hidden hover:-translate-y-1 transform transition '>
-                    <h3 className= " text-center text-lg md:text-xl mb-2 md:mb-1 font-semibold ">Add Assignment </h3>
-                  </Link>
-                ):(<></>)}
-                
-              </>
-            </div>
-        </div>
+    for(let i = 0; i < moduleArray.length; i++) {
+        let m = moduleArray[i];
 
-        <LoginCard></LoginCard>
-        <div className= "font-bold py-4 font-Dosis text-center text-4xl text-slate-400">Module: {moduleTitle} </div>
-        {nestedPage === "notifications" ? (
-        <Notifications/>
-        ): nestedPage === "viewfeedback" ? (
-          <ViewFeedback></ViewFeedback>
-        ): nestedPage === "submitwork" ? (
-          <Works></Works>
-        ): //default
-        (  <div className= "ml-80 mr-80 py-32 font-Dosis font-bold text-center text-8xl text-slate-200 border-dashed border-4 rounded border-slate-200">Select a tab on the side bar to view here!  </div>)
+        if(m.moduleId == id) {
+            //calcTimeLeft(w.dueDate);
+            return m;
         }
+    }
+  };
+
+  useEffect(() => {
+    const onPageLoad = () => {
+
+        //Debugging
+        console.log(params.id);
+        setModuleDetails(getModuleDetails(params.id));
+        console.log(getModuleDetails(params.id));
+         
+        // //console.log(userData);
+    };
+
+    if(document.readyState === 'complete') {
+        onPageLoad();
+    } else {
+        window.addEventListener('load', onPageLoad);
+        return () => window.removeEventListener('load', onPageLoad);
+    }
+}, []);
+
+  return(
+    <>
+    
+    <HeroSection prevPageName = "Modules" prevUrl= "/modules"></HeroSection>
+ 
+      <div className = 'font-Dosis bg:white dark:bg-gray-400'>
+        <div className='max-w-5xl mx-auto w-11/12'>
+              
+          <>
+          <SideBar moduleTitle={module.moduleName}> </SideBar>
+                
+              
+          {false === false ? ( // CHANGE TO ONLY APPEAR IF TEACHER !!!!!!!
+            <Link to= "/addassignment" className='sidebar fixed lg:left-0 text-gray-300 dark:text-gray-300 p-3 my-80 ml-2 py-6 bg-slate-800 dark:bg-zinc-700 w-72 h-20 overflow-hidden hover:-translate-y-1 transform transition '>
+            <h3 className= " text-center text-lg md:text-xl mb-2 md:mb-1 font-semibold ">Add Assignment </h3>
+            </Link>
+          ):(<></>)}
+                
+          </>
+        </div>
+      </div>
+
+      <LoginCard></LoginCard>
+      <div className= "font-bold py-4 font-Dosis text-center text-4xl text-slate-400">Module: {module.moduleName} </div>
+      {nestedPage === "notifications" ? (
+        <Notifications/>
+      ): nestedPage === "viewfeedback" ? (
+        <ViewFeedback></ViewFeedback>
+      ): nestedPage === "submitwork" ? (
+        <Works></Works>
+      ): //default
+      (  <div className= "ml-80 mr-80 py-32 font-Dosis font-bold text-center text-8xl text-slate-200 border-dashed border-4 rounded border-slate-200">Select a tab on the side bar to view here!  </div>)
+      }
 
         
        
-        <Routes> <Route path='/submitwork' element={<SubmitWork />} /></Routes>
-        <Routes> <Route path='/addassignment' element={<AddAssignment />} /></Routes>
-        {/* adds a route to redirect to submitwork page */}
-      </>
-    )
+      <Routes> <Route path='/submitwork' element={<SubmitWork />} /></Routes>
+      <Routes> <Route path='/addassignment' element={<AddAssignment />} /></Routes>
+      {/* adds a route to redirect to submitwork page */}
+    </>
+  )
 
     
 }
