2cd23bdd3daa1368a6044b8ff465fb4ca017c6fc
Vendron <hello@vendron.org>
2023-03-02 22:36:27 +0000

    update admin with getAllAdmins

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -5,11 +5,11 @@ import auth from "../middleware/auth.js";
 import adminController from "../controllers/adminController.js";
 import userController from "../controllers/userController.js";
 
-router.post("/admin", adminController.createAdmin); // create admin
+router.post("/admin/register", adminController.createAdmin); // create admin
 router.post("/admin/login", adminController.loginAdmin); // login admin
 router.get("/admin/me", auth, adminController.getAdmin); // get admin
 router.delete("/admin/me", auth, adminController.deleteAdmin); // delete admin
 
-router.get("/admin/getall", userController.getAllUsers); // get all users
-
+router.get("/admin/getallusers", userController.getAllUsers); // get all users
+router.get("/admin/getalladmins", adminController.getAllAdmins); // get all admins
 export default router;
