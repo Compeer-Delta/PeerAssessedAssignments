33ee9c8a9b43b9636f397dd07c98e9f5d7b0b16a
Vendron <hello@vendron.org>
2023-03-02 14:27:02 +0000

    add assignment schema

---
+++ api/src/schemas/assignment.js
@@ -0,0 +1,73 @@
+//ref: https://discord.com/channels/1023971333706629200/1025103915508838480/1049676345514393650
+//created by & Last update: 03/02, gc436
+import { Schema, model } from "mongoose";
+import { generateUUID } from "../functions/generateUUID.js";
+
+const asssignmentSchema = new Schema(
+  {
+    _id: Schema.Types.ObjectId,
+    assignmentId: {
+      type: String,
+      required: true,
+      default: generateUUID,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    bref: {
+      type: String,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    numOfReviewers:{
+        type: Number,
+        required: true,
+        default: 1,
+    },
+    imageULR:{
+        type: String,
+        required: true,
+        default: null,
+    },
+    teachers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        index: true,
+      },
+    ],
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        index: true,
+      },
+    ],
+    submissions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Submission",
+        index: true,
+      },
+    ],
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  }
+);
+
+export default model("Assignment", asssignmentSchema, "assignment");
