fbb497888347f7085553f743c25ed2bca621def3
Vendron <hello@vendron.org>
2023-03-02 15:49:10 +0000

    update index with body parser

--- api/src/index.js
+++ api/src/index.js
@@ -6,6 +6,7 @@ const { DB_SECRET, PORT } = process.env;
 import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 // import routes
 import userRoutes from "./routes/user.js";
 import moduleRoutes from "./routes/module.js";
@@ -15,7 +16,7 @@ import adminRoutes from "./routes/admin.js";
 
 const app = express();
 
-app.use(express.json(), cors());
+app.use(express.json(), bodyParser.json(), cors());
 
 //routes
 app.use(
@@ -27,9 +28,31 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("COMPEER API is live");
+  res.status(200).send("COMPEER API is live");
 });
 
+app.get("/battery", (req, res) => {
+  res.send("battery is live")
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send({ message: "User created" });
+  } catch (err) {
+    console.log(err.message)
+    res.status(400).send({ message: err.message });
+  }
+});
+/*
+  username: "test";
+  password: "testing";
+  firstname: "bob";
+  surname: "smith";
+  email: "bob@email.com";
+  institution: "university of test";
+  role: "student";*/
 //connect to database
 mongoose.set("strictQuery", false);
 mongoose.connect(DB_SECRET, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
