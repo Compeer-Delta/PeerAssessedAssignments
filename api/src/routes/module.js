6b2d2ee901db3c45f6cdfc7313049cadda5d1a4d
Vendron <hello@vendron.org>
2023-03-02 11:37:26 +0000

    update module routes syntax

--- api/src/routes/module.js
+++ api/src/routes/module.js
@@ -5,12 +5,12 @@ import auth from "../middleware/auth.js";
 import moduleController from "../controllers/moduleController.js";
 
 //routes for modules
-router.post("/module/create", auth.authUser, moduleController.createModule); //create module
-router.get("/module/:id", auth.authUser, moduleController.getModule); //get module
-router.patch("/module/:id", auth.authUser, moduleController.updateModule); //update module
-router.delete("/module/:id", auth.authUser, moduleController.deleteModule); //delete module
+router.post("/module/create", auth, moduleController.createModule); //create module
+router.get("/module/:id", auth, moduleController.getModule); //get module
+router.patch("/module/:id", auth, moduleController.updateModule); //update module
+router.delete("/module/:id", auth, moduleController.deleteModule); //delete module
 
-router.get("/modules", auth.authUser, moduleController.getModule); //get all modules
+router.get("/modules", auth, moduleController.getModule); //get all modules
 
 //routes for module submissions
 /*router.post("/modules/:id/submit", auth, moduleController.getModules); //submit assignment
@@ -19,12 +19,12 @@ router.patch("/module/:id", auth, moduleController.update); //update module subm
 router.delete("/module/:id", auth, moduleController.delete); //delete module submission*/
 
 //routes for module feedback
-router.post("/module/:id/feedback/submit", auth.authUser, moduleController.giveFeedback); //submit feedback
-router.get("/module/:id/feedback/view", auth.authUser, moduleController.viewFeedback); //view feedback
-router.delete("/module/:id/feedback/submit", auth.authUser, moduleController.deleteFeedback); //submit feedback
+router.post("/module/:id/feedback/submit", auth, moduleController.giveFeedback); //submit feedback
+router.get("/module/:id/feedback/view", auth, moduleController.viewFeedback); //view feedback
+router.delete("/module/:id/feedback/submit", auth, moduleController.deleteFeedback); //submit feedback
 
 //routes for module submissions (admin)
-router.get("/module/:id/submissions", auth.authUser, moduleController.getModules); //view all submissions
-router.get("/module/:id/submissions", auth.authUser, moduleController.getModules); //view submission
+router.get("/module/:id/submissions", auth, moduleController.getModules); //view all submissions
+router.get("/module/:id/submissions", auth, moduleController.getModules); //view submission
 
 export default router;
