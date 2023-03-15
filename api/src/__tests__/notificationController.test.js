b0a8eaf77e2c0a610c21be91b87013a567da8bf9
Vendron <gjc@clews.biz>
2023-03-15 08:29:57 -0700

    add notification test

---
+++ api/src/__tests__/notificationController.test.js
@@ -0,0 +1,50 @@
+import { it, expect, describe } from '@jest/globals';
+import { notificationController } from '../controllers/notificationController';
+import { Notification } from '../models/notificationModel';
+import supertest from 'supertest';
+const request = supertest('http://localhost:8080/api');
+
+const userId = 'test'; // replace with test user id
+const messageId = 'test'; // replace with test message id
+
+// create test notification
+beforeAll(async () => {
+    // create test notification
+    await notificationController.createNotif({
+        userId: userId,
+        notifTitle: 'Test Title',
+        notifContent: 'Test Content',
+        urgency: true
+    });
+});
+
+// delete test notification
+afterAll(async () => {
+    // delete test notification
+    await notificationController.deleteNotif({
+        userId, messageId
+    });
+});
+
+// test notification controller
+describe('notificationController', () => {
+
+    // create notification test data
+    it('PUSH notification test data', async () => {
+        const notif = await notificationController.createNotif({
+            userId: userId,
+            notifTitle: 'Test Title',
+            notifContent: 'Test Content',
+            urgency: true
+        }); // replace with appropriate query
+        expect(notif).toBeInstanceOf(Notification);
+    });
+
+    it('GET notification test data', async () => {
+        const notif = await notificationController.getNotif({
+            userId: userId,
+            messageId: messageId
+        }); // replace with appropriate query
+        expect(notif).toBeInstanceOf(Notification); // replace with being exactly equal to test data
+    });
+});
