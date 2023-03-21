1f9c5d107f9be483862fc0792aaaebb331150677
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-03-21 15:59:27 +0000

    General clean up in FileUploader.jsx

--- client/src/components/FileUploader.jsx
+++ client/src/components/FileUploader.jsx
@@ -9,46 +9,43 @@ function FileUploader(props, uploadType) {
     const [formattedData, setFormattedData] = useState([]);
 
     const handleFileChange = e => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = () => {
-            setFile({fileName: file.name, fileContent: reader.result});
+//         const file = e.target.files[0];
+//         const reader = new FileReader();
+//         reader.readAsText(file);
+//         reader.onload = () => {
+//             setFile({fileName: file.name, fileContent: reader.result});
            
- /// if ((((fileContent.split(/\n/))[0]).split(",")).length() == 4) //checks if its formatted in account data
-           if (props.uploadType === "modules")
-            {
-                props.UploadedData((reader.result).split(","))
-                console.log("in");
-            }
-            else if (props.uploadType === "accounts")
-            {
-                if ((((((reader.result).split(/\n/))[0]).split(",")).length == 5))
-                {
-                    console.log((((reader.result).split(/\n/))[0]).split(","))
+//   if ((((fileContent.split(/\n/))[0]).split(",")).length() == 4) //checks if its formatted in account data
+//            if (props.uploadType === "modules")
+//             {
+//                 props.UploadedData((reader.result).split(","))
+//                 console.log("in");
+//             }
+//             else if (props.uploadType === "accounts")
+//             {
+//                 if ((((((reader.result).split(/\n/))[0]).split(",")).length == 5))
+//                 {
+//                     console.log((((reader.result).split(/\n/))[0]).split(","))
                   
-                    props.UploadedData(((reader.result).split(/\n/)[0]).split(","))
+//                     props.UploadedData(((reader.result).split(/\n/)[0]).split(","))
                     
-                }
-                else
-                { console.log("error with size of row not equal to 4" + ((((reader.result).split(/\n/))[0]).split(",")))}
+//                 }
+//                 else
+//                 { console.log("error with size of row not equal to 4" + ((((reader.result).split(/\n/))[0]).split(",")))}
 
-            }
+//             }
 
-        }
-        reader.onerror= () => {
- 		console.log("file error", reader.error)
-        }
+//         }
+//         reader.onerror= () => {
+//  		console.log("file error", reader.error)
+//         }
         
     }
    
     return (
         <div>
-            
-            
             <input type="file" onChange={handleFileChange} ></input>
             <br/><br/>
- 
         </div>
     )
   
