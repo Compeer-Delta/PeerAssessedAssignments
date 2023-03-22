f985faa0d5f329fd0e8224e02dbe6abd70bd5dd9
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-03-22 17:35:08 +0000

    Adding support for a passed array

--- client/src/components/Works.jsx
+++ client/src/components/Works.jsx
@@ -3,28 +3,23 @@ import React, { useState } from 'react'
 import WorkItem from './WorkItem'
 import works from '../data/works'
 
-function Works({modulename}) {
-    
-    //DB read in to display only assignmnets that match module id
-    var assignments = works;
+function Works({ assignments }) {
 
-    function sortByLastId(assignments)
-    {//sorts assignments from the biggest id to the smallest id, bigger ids are assignments that were set later
-        let sortedAssignments;
-        sortedAssignments = assignments.sort(function(a,b){ return b.id - a.id;})
-        return sortedAssignments
-    }
+    // function sortByLastId(assignments)
+    // {//sorts assignments from the biggest id to the smallest id, bigger ids are assignments that were set later
+    //     let sortedAssignments;
+    //     sortedAssignments = assignments.sort(function(a,b){ return b.id - a.id;})
+    //     return sortedAssignments
+    // }
     
-    var sorted = sortByLastId(assignments);
-    
-    
+    // var sorted = sortByLastId(assignments);
 
     return (
         <div className = 'py-2 dark:bg-zinc-900'>
           {
             }
             <div className='pr-80 lg:pl-80 pl-16 grid grid-cols-1 gap-3'>
-                {sorted.map(work => (
+                {assignments.map(work => (
                     <WorkItem key={work.id}
                               id={work.id}
                               imgUrl={work.imgUrl}
