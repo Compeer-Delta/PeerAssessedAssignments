1c96eecf2be40feb6595139ed6e2e86f5beeae52
Vendron <hello@vendron.org>
2023-03-25 15:29:28 +0000

    update getAssignment route

--- api/src/routes/assignment.js
+++ api/src/routes/assignment.js
@@ -5,8 +5,8 @@ import auth from "../middleware/auth.js";
 import assignmentController from "../controllers/assignmentController.js";
 
 //routes for assignments
-router.post("/assignment/upload/", assignmentController.addAssignment); //upload assignment
-router.get("/assignment/", auth, assignmentController.getAssignment); //get assignment
+router.post("/assignment/upload/", auth, assignmentController.addAssignment); //upload assignment
+router.get("/assignment/:moduleId/:assignmentId", auth, assignmentController.getAssignment); //get assignment
 router.patch("/assignment/", auth, assignmentController.updateAssignment); //update assignment
 router.delete("/assignment/", auth, assignmentController.deleteAssignment); //delete assignment
 
