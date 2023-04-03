c6c688ee0e776678b6e9706495ee36edea2604fe
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-04-03 19:08:26 +0100

    Input fields added

--- client/src/pages/AddNotification.jsx
+++ client/src/pages/AddNotification.jsx
@@ -15,15 +15,22 @@ function AddNotification(props) {
 
   const [userId, setUserId] = useState(ReactSession.get("uid"));
 
+  const [notifTitle, setNotifTitle] = useState("");
+  const [notifContent, setNotifContent] = useState("");
+  const [notifUrgency, setNotifUrgency] = useState(false);
+
   const addNewNotif = async (e) => {
     // Add a new notification
     const response = await addNotification(
-
+      
     );
     const details = await response.json();
     console.log(details);
   };
 
+  const toggleNewNotif = () => {
+
+  }
 
   return (
     <>
@@ -33,7 +40,51 @@ function AddNotification(props) {
       <div className="py-3 dark:bg-zinc-900 h-screen">
         <h1 className=" font-Dosis sm:ml-80 ml-32 text-3xl text-slate-600 font-semibold dark:text-white rounded-md pb-4"></h1>
         <div className=" font-Dosis  2xl:w-[1200px] w-screen text-xl border-2 border-slate-700 rounded  2xl:ml-80 2xl:mr-80 bg-slate-300 py-10">
-          
+          {confirmedAssignment === true ? (
+            <>
+              <p className="text-center bg-green-200">
+                You have added a new notification
+              </p>
+              <button
+                onClick={toggleNewNotif}
+                className="mt-10 ml-48 shadow bg-green-700 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded "
+              >
+                Add New Notification
+              </button>{" "}
+            </>
+          ) : (
+            <div className="pl-10">
+              <h1 className=" ml-8 text-l   text-slate-600 font-semibold dark:text-white rounded-md ">
+                {" "}
+                Subject:{" "}
+              </h1>
+
+              <input
+                onChange={(e) => setNotifTitle(e.target.value)}
+                value={notifTitle}
+                name="notifT"
+                required
+                className="ml-8  mb-10 bg-slate-100 appearance-none border-2 border-slate-200 rounded w-[260px] sm:w-[500px] py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
+                id="notifT"
+                type="text"
+                placeholder="Notification Subject"
+              ></input>
+              <h1 className=" ml-8  text-l  text-slate-600 font-semibold dark:text-white rounded-md ">
+                {" "}
+                Description:{" "}
+              </h1>
+              <textarea
+                onChange={(e) => setNotifContent(e.target.value)}
+                value={notifContent}
+                name="notifC"
+                required
+                id="notifC"
+                rows="4"
+                className="ml-8  block p-2.5 w-[260px] sm:w-[500px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your notification content..."
+              ></textarea>
+            </div>
+          )}
         </div>
         { /*Submit Button*/ }
         <div className="ml-8 md:flex md:items-center py-5">
