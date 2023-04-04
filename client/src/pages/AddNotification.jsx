17add09ccccb78b3005dda1989c1bdfa84837816
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2023-04-04 15:13:46 +0100

    Updates to AddNotification page

--- client/src/pages/AddNotification.jsx
+++ client/src/pages/AddNotification.jsx
@@ -1,36 +1,54 @@
-import React from "react";
-import Modules from "../pages/Modules";
-import HeroSection from "../components/HeroSection";
-import { Link } from "react-router-dom";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useState } from "react";
-import StudentView from "./StudentView";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import SubmitWork from "./SubmitWork";
-import FileUploader from "../components/FileUploader";
 import "../functions/api/notificationAPI";
 import { ReactSession } from "react-client-session";
+import ModuleSearchBox from "../components/ModuleSearchBox";
+import { getUserModules } from "../functions/api/moduleAPI";
+import { createNotification } from "../functions/api/notificationAPI";
 
 function AddNotification(props) {
 
   const [userId, setUserId] = useState(ReactSession.get("uid"));
+  const [userInst, setUserInst] = useState(ReactSession.get("inst"));
+  const [userEmail, setUserEmail] = useState(ReactSession.get("email"));
 
   const [notifTitle, setNotifTitle] = useState("");
   const [notifContent, setNotifContent] = useState("");
-  const [notifUrgency, setNotifUrgency] = useState(false);
+  const [notifUrgency, setNotifUrgency] = useState(false); //Not urgent by default
+  const [moduleGroups, setModuleGroups] = useState(null);
+
+  const [confirmedNotification, setConfirmedNotification] = useState(false);
 
   const addNewNotif = async (e) => {
     // Add a new notification
-    const response = await addNotification(
+    const response = await createNotification(
       
     );
     const details = await response.json();
     console.log(details);
+
+    setConfirmedNotification(true);
   };
 
   const toggleNewNotif = () => {
-
+    setNotifContent("");
+    setNotifTitle("");
+    setNotifUrgency(false);
+    setConfirmedNotification(false);
   }
+
+  useLayoutEffect(() => {
+    const getModuleGroups = async () => {
+      const response = await getUserModules(userEmail, ReactSession.get("token"));
+      const details = await response.json();
+
+      setModuleGroups(details);
+    };
+
+    getModuleGroups();
+    //console.log(moduleGroups);
+  }, [])
 
   return (
     <>
@@ -38,15 +56,18 @@ function AddNotification(props) {
       <script src="../path/to/flowbite/dist/datepicker.js"></script>
 
       <div className="py-3 dark:bg-zinc-900 h-screen">
-        <h1 className=" font-Dosis sm:ml-80 ml-32 text-3xl text-slate-600 font-semibold dark:text-white rounded-md pb-4"></h1>
+      <h1 className=" font-Dosis sm:ml-80 ml-32 text-3xl text-slate-600 font-semibold dark:text-white rounded-md pb-4">
+          {" "}
+          Add Notification:
+        </h1>
         <div className=" font-Dosis  2xl:w-[1200px] w-screen text-xl border-2 border-slate-700 rounded  2xl:ml-80 2xl:mr-80 bg-slate-300 py-10">
-          {confirmedAssignment === true ? (
+          {confirmedNotification === true ? (
             <>
               <p className="text-center bg-green-200">
                 You have added a new notification
               </p>
               <button
-                onClick={toggleNewNotif}
+                onClick={(e) => toggleNewNotif()}
                 className="mt-10 ml-48 shadow bg-green-700 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded "
               >
                 Add New Notification
@@ -58,7 +79,6 @@ function AddNotification(props) {
                 {" "}
                 Subject:{" "}
               </h1>
-
               <input
                 onChange={(e) => setNotifTitle(e.target.value)}
                 value={notifTitle}
@@ -69,6 +89,7 @@ function AddNotification(props) {
                 type="text"
                 placeholder="Notification Subject"
               ></input>
+
               <h1 className=" ml-8  text-l  text-slate-600 font-semibold dark:text-white rounded-md ">
                 {" "}
                 Description:{" "}
@@ -81,8 +102,44 @@ function AddNotification(props) {
                 id="notifC"
                 rows="4"
                 className="ml-8  block p-2.5 w-[260px] sm:w-[500px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Write your notification content..."
+                placeholder="Write something to tell your students..."
               ></textarea>
+
+              <h1 className=" ml-8  text-l  text-slate-6 00 font-semibold dark:text-white rounded-md ">
+                {" "}
+                Send to:{" "}
+              </h1>
+              {moduleGroups && moduleGroups.length > 0 ? (
+              <>
+              <select name="modSelect" className="ml-8">
+                {moduleGroups.map((module) => {
+                  <option 
+                  value={module.students} 
+                  name={module.moduleCode}>
+                    {module.title}
+                  </option>
+                })} 
+              </select>
+              </>
+              )  : (
+              <></>
+              )}
+
+              <h1 className=" ml-8  text-l  text-slate-6 00 font-semibold dark:text-white rounded-md ">
+                {" "}
+                Urgent?{" "}
+              </h1>
+              <label className= "block text-sm">
+              <input
+                type = "checkbox"
+                className = "ml-8"
+                name = "notifU"
+                id = "notifU"
+                value = {notifUrgency}
+                onChange = {(e) => setNotifUrgency(!notifUrgency)}
+              ></input>
+              Tick if the notification is urgent.
+              </label>
             </div>
           )}
         </div>
@@ -90,7 +147,7 @@ function AddNotification(props) {
         <div className="ml-8 md:flex md:items-center py-5">
             <div className="md:w-2/3 ">
               <button
-                onClick={addNewNotif}
+                onClick={(e) => addNewNotif()}
                 className="ml-8 shadow bg-green-700 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded "
               >
               Add Notification
